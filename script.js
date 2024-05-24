b6c7da026ac1229edbf042c1bbd8862c0a651e1f
bird909 <birdseyefree@gmail.com>
2024-05-24 09:21:04 +0000

    Update file names and paths for GitHub Pages

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Healing WebApp Loaded');
+    const items = document.querySelectorAll('.item');
+    const audioPlayer = document.getElementById('audio-player');
+
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            const musicSrc = item.getAttribute('data-music');
+            audioPlayer.src = musicSrc;
+            audioPlayer.style.display = 'block';
+            audioPlayer.play();
+        });
+    });
 });
