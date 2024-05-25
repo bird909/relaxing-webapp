4190893d9a68526271274c3194ed131c04f96070
bird909 <birdseyefree@gmail.com>
2024-05-25 06:44:26 +0000

    Update category pages and main page with new structure

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const overlay = document.getElementById('overlay');
-    const overlayImage = document.getElementById('overlay-image');
-    const overlayAudio = document.getElementById('overlay-audio');
-    const overlayQuoteText = document.getElementById('overlay-quote-text');
-    const overlayAuthorText = document.getElementById('overlay-author-text');
-    const overlayTranslationText = document.getElementById('overlay-translation-text');
-    const closeBtn = document.getElementById('close-btn');
     const items = document.querySelectorAll('.item');
-
-    let quotes = [];
-
-    // JSON 파일에서 명언 로드
-    fetch('healing_quotes.json')
-        .then(response => response.json())
-        .then(data => {
-            quotes = data;
-            items.forEach((item, index) => {
-                const button = item.querySelector('.transparent-btn');
-                button.addEventListener('click', () => handleItemClick(item));
-                button.addEventListener('touchstart', () => handleItemClick(item));
-            });
-        });
+    const overlay = document.getElementById('overlay');
+    const overlayImage = document.getElementById('overlayImage');
+    const overlayAudio = document.getElementById('overlayAudio');
+    const overlayQuoteText = document.getElementById('overlayQuoteText');
+    const closeBtn = document.getElementById('close-btn');
 
     const handleItemClick = (item) => {
         const musicSrc = item.getAttribute('data-music');
         const imgSrc = item.querySelector('img').src;
-
-        // 랜덤 명언 선택
-        const randomIndex = Math.floor(Math.random() * quotes.length);
-        const quote = quotes[randomIndex];
+        const quote = item.getAttribute('data-quote');
 
         overlayImage.src = imgSrc;
         overlayAudio.src = musicSrc;
         overlayAudio.style.display = 'block';
         overlayAudio.play();
-        overlayQuoteText.textContent = quote.quote;
-        overlayAuthorText.textContent = `- ${quote.author}`;
-        overlayTranslationText.textContent = quote.translation;
+        overlayQuoteText.textContent = quote;
 
         overlay.style.display = 'flex';
     };
+
+    items.forEach((item) => {
+        item.addEventListener('click', () => handleItemClick(item));
+        item.addEventListener('touchstart', () => handleItemClick(item));
+    });
 
     closeBtn.addEventListener('click', () => {
         overlay.style.display = 'none';
