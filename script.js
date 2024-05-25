4073b2421832aaf5e0b7e44ecc1fa5e1cca57572
bird909 <birdseyefree@gmail.com>
2024-05-25 07:24:52 +0000

    Add category pages and move images and audio files

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const items = document.querySelectorAll('.item');
-    const overlay = document.getElementById('overlay');
-    const overlayImage = document.getElementById('overlayImage');
-    const overlayAudio = document.getElementById('overlayAudio');
-    const overlayQuoteText = document.getElementById('overlayQuoteText');
-    const closeBtn = document.getElementById('close-btn');
 
-    const handleItemClick = (item) => {
-        const musicSrc = item.getAttribute('data-music');
-        const imgSrc = item.querySelector('img').src;
-        const quote = item.getAttribute('data-quote');
-
-        overlayImage.src = imgSrc;
-        overlayAudio.src = musicSrc;
-        overlayAudio.style.display = 'block';
-        overlayAudio.play();
-        overlayQuoteText.textContent = quote;
-
-        overlay.style.display = 'flex';
-    };
-
-    items.forEach((item) => {
-        item.addEventListener('click', () => handleItemClick(item));
-        item.addEventListener('touchstart', () => handleItemClick(item));
-    });
-
-    closeBtn.addEventListener('click', () => {
-        overlay.style.display = 'none';
-        overlayAudio.pause();
-        overlayAudio.currentTime = 0;
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            const link = item.getAttribute('data-link');
+            window.location.href = link;
+        });
     });
 });
