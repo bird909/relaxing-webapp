013201a4c5c0f4b364874d24fcbd66ad96cad983
bird909 <birdseyefree@gmail.com>
2024-05-25 09:25:30 +0000

    Update design

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const items = document.querySelectorAll('.item');
-    const overlay = document.getElementById('overlay');
-    const overlayImage = document.getElementById('overlayImage');
-    const overlayAudio = document.getElementById('overlayAudio');
-    const overlayQuoteText = document.getElementById('overlayQuoteText');
-    const closeBtn = document.getElementById('close-btn');
-
-    items.forEach(item => {
-        item.addEventListener('click', () => {
-            const musicSrc = item.getAttribute('data-music');
-            const imgSrc = item.querySelector('img').src;
-            const quote = item.getAttribute('data-quote');
-
-            overlayImage.src = imgSrc;
-            overlayAudio.src = musicSrc;
-            overlayQuoteText.textContent = quote;
-
-            overlay.style.display = 'flex';
-            overlayAudio.play();
-        });
+    
+    const handleItemClick = (item) => {
+        const musicSrc = item.getAttribute('data-music');
+        const imgSrc = item.querySelector('img').src;
+        const quote = item.getAttribute('data-quote');
+        const author = item.getAttribute('data-author');
+        const translation = item.getAttribute('data-translation');
+        
+        overlayImage.src = imgSrc;
+        overlayAudio.src = musicSrc;
+        overlayAudio.style.display = 'block';
+        overlayAudio.play();
+        overlayQuoteText.textContent = quote;
+        overlayQuoteAuthor.textContent = author;
+        overlayQuoteTranslation.textContent = translation;
+        
+        overlay.style.display = 'flex';
+    };
+    
+    items.forEach((item) => {
+        item.addEventListener('click', () => handleItemClick(item));
+        item.addEventListener('touchstart', () => handleItemClick(item));
     });
-
+    
     closeBtn.addEventListener('click', () => {
         overlay.style.display = 'none';
         overlayAudio.pause();
