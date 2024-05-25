db9305c6ba03ae28b034cc720236cddf54d35164
bird909 <birdseyefree@gmail.com>
2024-05-25 10:22:16 +0000

    Update design

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
     const items = document.querySelectorAll('.item');
+    const overlay = document.getElementById('overlay');
+    const overlayImage = document.getElementById('overlayImage');
+    const overlayAudio = new Audio();
+    const overlayQuoteText = document.getElementById('overlayQuoteText');
+    const overlayQuoteTranslation = document.getElementById('overlayQuoteTranslation');
+    const overlayQuoteAuthor = document.getElementById('overlayQuoteAuthor');
+    const closeBtn = document.getElementById('close-btn');
 
     const handleItemClick = (item) => {
         const musicSrc = item.getAttribute('data-music');
         const imgSrc = item.querySelector('img').src;
         const quote = item.getAttribute('data-quote');
-        const translation = item.getAttribute('data-translation');
         const author = item.getAttribute('data-author');
+        const translation = item.getAttribute('data-translation');
 
-        document.getElementById('overlayImage').src = imgSrc;
-        document.getElementById('overlayAudio').src = musicSrc;
-        document.getElementById('overlayQuoteText').textContent = quote;
-        document.getElementById('overlayQuoteTranslation').textContent = translation;
-        document.getElementById('overlayQuoteAuthor').textContent = author;
-        
-        document.getElementById('overlay').style.display = 'flex';
+        overlayImage.src = imgSrc;
+        overlayAudio.src = musicSrc;
+        overlayAudio.loop = true;
+        overlayAudio.play();
+        overlayQuoteText.textContent = quote;
+        overlayQuoteTranslation.textContent = translation;
+        overlayQuoteAuthor.textContent = `- ${author}`;
 
-        const audioElement = document.getElementById('overlayAudio');
-        audioElement.play();
-        audioElement.loop = true;
+        overlay.style.display = 'flex';
     };
 
-    items.forEach(item => {
+    items.forEach((item) => {
         item.addEventListener('click', () => handleItemClick(item));
-        item.addEventListener('touchstart', () => handleItemClick(item));
     });
 
-    document.getElementById('close-btn').addEventListener('click', () => {
-        document.getElementById('overlay').style.display = 'none';
-        const audioElement = document.getElementById('overlayAudio');
-        audioElement.pause();
-        audioElement.currentTime = 0;
+    closeBtn.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        overlayAudio.pause();
+        overlayAudio.currentTime = 0;
     });
 });
