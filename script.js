3dfabf386ebd29c4e41fac738bb10f2df775a7e6
bird909 <birdseyefree@gmail.com>
2024-05-26 04:21:45 +0000

    Update design

--- script.js
+++ script.js
@@ -4,31 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlayImage = document.getElementById('overlayImage');
     const overlayAudio = document.getElementById('overlayAudio');
     const overlayQuoteText = document.getElementById('overlayQuoteText');
-    const overlayQuoteTranslation = document.getElementById('overlayQuoteTranslation');
-    const overlayQuoteAuthor = document.getElementById('overlayQuoteAuthor');
     const closeBtn = document.getElementById('close-btn');
 
     const handleItemClick = (item) => {
         const musicSrc = item.getAttribute('data-music');
         const imgSrc = item.querySelector('img').src;
         const quote = item.getAttribute('data-quote');
-        const author = item.getAttribute('data-author');
-        const translation = item.getAttribute('data-translation');
 
         overlayImage.src = imgSrc;
         overlayAudio.src = musicSrc;
         overlayAudio.loop = true;
-        overlayAudio.autoplay = true;
         overlayAudio.play();
         overlayQuoteText.textContent = quote;
-        overlayQuoteTranslation.textContent = translation;
-        overlayQuoteAuthor.textContent = `- ${author}`;
 
         overlay.style.display = 'flex';
     };
 
     items.forEach((item) => {
         item.addEventListener('click', () => handleItemClick(item));
+        item.addEventListener('touchstart', () => handleItemClick(item));
     });
 
     closeBtn.addEventListener('click', () => {
