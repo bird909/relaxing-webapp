0070a384afb8a514a73fa2ee22d0efe8c927f7e1
bird909 <birdseyefree@gmail.com>
2024-05-26 01:29:44 +0000

    Update design

--- main.js
+++
@@ -1,7 +0,0 @@
-import { initializeGallery } from './modules/galleryInitializer.js';
-import { closeOverlay } from './modules/overlayHandler.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initializeGallery();
-    document.getElementById('close-btn').addEventListener('click', closeOverlay);
-});
---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.item');
+    const overlay = document.getElementById('overlay');
+    const overlayImage = document.getElementById('overlayImage');
+    const overlayAudio = document.getElementById('overlayAudio');
+    const overlayQuoteText = document.getElementById('overlayQuoteText');
+    const closeBtn = document.getElementById('close-btn');
+
+    const handleItemClick = (item) => {
+        const musicSrc = item.getAttribute('data-music');
+        const imgSrc = item.querySelector('img').src;
+        const quote = item.getAttribute('data-quote');
+
+        overlayImage.src = imgSrc;
+        overlayAudio.src = musicSrc;
+        overlayAudio.loop = true;
+        overlayAudio.play();
+        overlayQuoteText.textContent = quote;
+
+        overlay.style.display = 'flex';
+    };
+
+    items.forEach((item) => {
+        item.addEventListener('click', () => handleItemClick(item));
+        item.addEventListener('touchstart', () => handleItemClick(item));
+    });
+
+    closeBtn.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        overlayAudio.pause();
+        overlayAudio.currentTime = 0;
+    });
+});
