de39af34b8806ded7c4524ba6aade287e09ff2fb
bird909 <birdseyefree@gmail.com>
2024-05-26 00:55:51 +0000

    Update design

--- overlayhandler.js
+++ overlayhandler.js
@@ -3,16 +3,22 @@ export const handleItemClick = (item) => {
     const overlayImage = document.getElementById('overlayImage');
     const overlayAudio = document.getElementById('overlayAudio');
     const overlayQuoteText = document.getElementById('overlayQuoteText');
+    const overlayQuoteTranslation = document.getElementById('overlayQuoteTranslation');
+    const overlayQuoteAuthor = document.getElementById('overlayQuoteAuthor');
 
     const musicSrc = item.getAttribute('data-music');
     const imgSrc = item.querySelector('img').src;
     const quote = item.getAttribute('data-quote');
+    const translation = item.getAttribute('data-translation');
+    const author = item.getAttribute('data-author');
 
     overlayImage.src = imgSrc;
     overlayAudio.src = musicSrc;
     overlayAudio.loop = true;
     overlayAudio.play();
     overlayQuoteText.textContent = quote;
+    overlayQuoteTranslation.textContent = translation;
+    overlayQuoteAuthor.textContent = author;
 
     overlay.style.display = 'flex';
 };
