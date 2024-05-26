3291e69562b30c99e8782d8d70de048384c1a2e1
bird909 <birdseyefree@gmail.com>
2024-05-26 00:20:41 +0000

    Update design

--- script.js
+++ script.js
@@ -4,23 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlayImage = document.getElementById('overlayImage');
     const overlayAudio = document.getElementById('overlayAudio');
     const overlayQuoteText = document.getElementById('overlayQuoteText');
-    const overlayQuoteTranslation = document.getElementById('overlayQuoteTranslation');
-    const overlayQuoteAuthor = document.getElementById('overlayQuoteAuthor');
     const closeBtn = document.getElementById('close-btn');
 
     const handleItemClick = (item) => {
         const musicSrc = item.getAttribute('data-music');
         const imgSrc = item.querySelector('img').src;
         const quote = item.getAttribute('data-quote');
-        const translation = item.getAttribute('data-translation');
-        const author = item.getAttribute('data-author');
 
         overlayImage.src = imgSrc;
         overlayAudio.src = musicSrc;
+        overlayAudio.loop = true;
         overlayAudio.play();
         overlayQuoteText.textContent = quote;
-        overlayQuoteTranslation.textContent = translation;
-        overlayQuoteAuthor.textContent = author;
 
         overlay.style.display = 'flex';
     };
@@ -34,10 +29,5 @@ document.addEventListener('DOMContentLoaded', () => {
         overlay.style.display = 'none';
         overlayAudio.pause();
         overlayAudio.currentTime = 0;
-    });
-
-    overlayAudio.addEventListener('ended', () => {
-        overlayAudio.currentTime = 0;
-        overlayAudio.play();
     });
 });
