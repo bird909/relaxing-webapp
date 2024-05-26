068ae7ec78ba8e4cfcd852aa4db5015960b9a5ff
bird909 <birdseyefree@gmail.com>
2024-05-26 04:15:58 +0000

    Update design

--- script.js
+++ script.js
@@ -7,42 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlayQuoteTranslation = document.getElementById('overlayQuoteTranslation');
     const overlayQuoteAuthor = document.getElementById('overlayQuoteAuthor');
     const closeBtn = document.getElementById('close-btn');
-    
-    let quotes = [];
-
-    // Load quotes from the JSON file
-    fetch('../quotes.json')
-        .then(response => response.json())
-        .then(data => {
-            quotes = data;
-        })
-        .catch(error => console.error('Error loading quotes:', error));
 
     const handleItemClick = (item) => {
         const musicSrc = item.getAttribute('data-music');
         const imgSrc = item.querySelector('img').src;
-        const quoteIndex = item.getAttribute('data-quote-index');
-        
-        // Find the current category based on the document title
-        const category = document.title.split(' - ')[1].toLowerCase().replace(' ', '');
-
-        // Find the correct quote based on the category and quoteIndex
-        const categoryQuotes = quotes.find(q => q.category === category).quotes;
-        const quote = categoryQuotes[quoteIndex];
+        const quote = item.getAttribute('data-quote');
+        const author = item.getAttribute('data-author');
+        const translation = item.getAttribute('data-translation');
 
         overlayImage.src = imgSrc;
         overlayAudio.src = musicSrc;
-        overlayQuoteText.textContent = quote.quote;
-        overlayQuoteTranslation.textContent = quote.translation;
-        overlayQuoteAuthor.textContent = `- ${quote.author}`;
-        
-        overlay.style.display = 'flex';
+        overlayAudio.loop = true;
+        overlayAudio.autoplay = true;
         overlayAudio.play();
+        overlayQuoteText.textContent = quote;
+        overlayQuoteTranslation.textContent = translation;
+        overlayQuoteAuthor.textContent = `- ${author}`;
+
+        overlay.style.display = 'flex';
     };
 
     items.forEach((item) => {
         item.addEventListener('click', () => handleItemClick(item));
-        item.addEventListener('touchstart', () => handleItemClick(item));
     });
 
     closeBtn.addEventListener('click', () => {
