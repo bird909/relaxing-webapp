b6f4aace650d8c5d78482beebcdf94b6f7f79c99
bird909 <birdseyefree@gmail.com>
2024-05-26 03:33:24 +0000

    Update design

--- script.js
+++ script.js
@@ -4,52 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlayImage = document.getElementById('overlayImage');
     const overlayAudio = document.getElementById('overlayAudio');
     const overlayQuoteText = document.getElementById('overlayQuoteText');
-    const overlayQuoteTranslation = document.getElementById('overlayQuoteTranslation');
-    const overlayQuoteAuthor = document.getElementById('overlayQuoteAuthor');
     const closeBtn = document.getElementById('close-btn');
 
-    let tracks = [];
+    const handleItemClick = (item) => {
+        const musicSrc = item.getAttribute('data-music');
+        const imgSrc = item.querySelector('img').src;
+        const quote = item.getAttribute('data-quote');
 
-    // JSON 파일에서 데이터 로드
-    fetch('../quotes.json')
-        .then(response => response.json())
-        .then(data => {
-            const category = document.querySelector('body').dataset.category;
-            const categoryData = data.find(cat => cat.category === category);
-            tracks = categoryData ? categoryData.quotes : [];
-        })
-        .catch(error => console.error('Error loading quotes:', error));
-
-    const getRandomQuote = () => {
-        const randomIndex = Math.floor(Math.random() * tracks.length);
-        return tracks[randomIndex];
-    };
-
-    const loadTrack = (track) => {
-        const item = items[currentTrackIndex];
-        overlayImage.src = item.querySelector('img').src;
-        overlayAudio.src = track.music;
+        overlayImage.src = imgSrc;
+        overlayAudio.src = musicSrc;
+        overlayAudio.loop = true;
         overlayAudio.play();
-        overlayQuoteText.textContent = track.quote;
-        overlayQuoteTranslation.textContent = track.translation;
-        overlayQuoteAuthor.textContent = track.author;
-    };
+        overlayQuoteText.textContent = quote;
 
-    const handleItemClick = (index) => {
-        currentTrackIndex = index;
-        const track = getRandomQuote();
-        loadTrack(track);
         overlay.style.display = 'flex';
     };
 
-    items.forEach((item, index) => {
-        item.addEventListener('click', () => handleItemClick(index));
-        item.addEventListener('touchstart', () => handleItemClick(index));
-    });
-
-    overlayAudio.addEventListener('ended', () => {
-        const track = getRandomQuote();
-        loadTrack(track);
+    items.forEach((item) => {
+        item.addEventListener('click', () => handleItemClick(item));
+        item.addEventListener('touchstart', () => handleItemClick(item));
     });
 
     closeBtn.addEventListener('click', () => {
